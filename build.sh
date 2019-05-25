4466cd03abc108e7ecd5a2b304a21b7434760286
John Bytheway <jbytheway@gmail.com>
2019-05-25 11:40:46 +0100

    Run cmake in proper place

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ set -uex
 export CC=clang-8
 export CXX=clang++-8
 
-root=$PWD
 alias git_clone="git clone --depth 1 --branch release_80"
 
 git_clone https://github.com/llvm-mirror/clang.git
 cd clang/tools
 git_clone https://git.llvm.org/git/clang-tools-extra.git extra
-cd "$root"
+cd ..
 mkdir build
 cd build
 cmake \
