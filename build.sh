9db7375d1cb25799198e1be9595ebb107603303f
John Bytheway <jbytheway@gmail.com>
2019-05-25 11:40:00 +0100

    Use shallow clones of specific branch

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ export CC=clang-8
 export CXX=clang++-8
 
 root=$PWD
+alias git_clone="git clone --depth 1 --branch release_80"
 
-git clone https://github.com/llvm-mirror/clang.git
+git_clone https://github.com/llvm-mirror/clang.git
 cd clang/tools
-git clone https://git.llvm.org/git/clang-tools-extra.git extra
+git_clone https://git.llvm.org/git/clang-tools-extra.git extra
 cd "$root"
 mkdir build
 cd build
