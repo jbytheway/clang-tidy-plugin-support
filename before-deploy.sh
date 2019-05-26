3d2a9bb3010ac7729c2188014f06ac4cf886fcc7
John Bytheway <jbytheway@gmail.com>
2019-05-26 21:33:54 +0100

    Move deploy prep to separate script

---
+++ before-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=clang-tidy-plugin-support-$TRAVIS_TAG
+mkdir -p $name/{include,bin}
+cp clang/build/bin/clang-tidy $name/bin
+cp clang/tools/extra/clang-tidy/*.h $name/include
+cp clang/LICENSE.TXT $name/clang-LICENSE.TXT
+tar -cJvf $name.tar.xz $name
+
