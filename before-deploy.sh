a8afa1aa4e304ec5d13577c65a11519f6d2599f4
John Bytheway <jbytheway@gmail.com>
2019-05-27 09:17:53 +0100

    Set up symlink for system includes
    We need our clang-tidy to be able to access system includes.  Try to do
    that by adding a symlink in a suitable spot.

--- before-deploy.sh
+++ before-deploy.sh
@@ -3,8 +3,9 @@
 set -uex
 
 name=clang-tidy-plugin-support-$TRAVIS_TAG
-mkdir -p $name/{include,bin}
+mkdir -p $name/{include,bin,lib}
 cp clang/build/bin/clang-tidy $name/bin
 cp clang/tools/extra/clang-tidy/*.h $name/include
 cp clang/LICENSE.TXT $name/clang-LICENSE.TXT
+ln -s /usr/include/clang $name/lib/clang
 tar -cJvf $name.tar.xz $name
