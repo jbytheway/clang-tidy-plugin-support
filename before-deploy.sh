29869d34d9529af46c6220be518660db1f62d0a5
John Bytheway <jbytheway@gmail.com>
2019-05-27 16:05:51 +0100

    Include FileCheck symlink
    To allow clang_tidy_check.py to work

--- before-deploy.sh
+++ before-deploy.sh
@@ -6,6 +6,7 @@ name=clang-tidy-plugin-support-$TRAVIS_TAG
 mkdir -p $name/{include,bin,lib}
 cp clang/build/bin/clang-tidy $name/bin
 cp clang/tools/extra/test/clang-tidy/check_clang_tidy.py $name/bin
+ln -s /bin/FileCheck-8 $name/bin/FileCheck
 cp clang/tools/extra/clang-tidy/*.h $name/include
 cp clang/LICENSE.TXT $name/clang-LICENSE.TXT
 ln -s /usr/include/clang $name/lib/clang
